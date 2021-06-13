818e5b437ee7b9e86845145eb628720145c6341c
nakarun <etd7crnt@gmail.com>
2021-06-13 16:46:28 +0900

    クラスベースビューへのリファクタ

--- hairsalon/views.py
+++ hairsalon/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render, get_object_or_404
+from django.views import View
 
 from .models import News
 
 # Create your views here.
 
-def top(request):
-    news = News.objects.order_by('-published')
-    return render(request, 'hairsalon/top.html', {'news': news, 'current_menu_item': 'top'})
+class TopView(View):
+    def get(self, request, *args, **kwargs):
+        news = News.objects.order_by('-published')
+        context = {
+            'news': news,
+            'current_menu_item': 'top',
+        }
+        return render(request, 'hairsalon/top.html', context)
 
-def news_detail(request, news_id):
-    news_detail = get_object_or_404(News, pk=news_id)
-    return render(request, 'hairsalon/news_detail.html', {'news_detail': news_detail, 'current_menu_item': 'news'})
+top = TopView.as_view()
+
+class NewsDetailView(View):
+    def get(self, request, news_id, *args, **kwargs):
+        news_detail = get_object_or_404(News, pk=news_id)
+        context = {
+            'news_detail': news_detail,
+            'current_menu_item': 'news',
+        }
+        return render(request, 'hairsalon/news_detail.html', context)
+
+news_detail = NewsDetailView.as_view()
