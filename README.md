77eeb5151a2a0f041d7952a24525ad084583d22c
nakarun <etd7crnt@gmail.com>
2021-06-13 15:36:01 +0900

    TOP PAGEの見た目を整える

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # 美容室用のHPをDjangoで作成してみました。
+
+# デザイン
+https://f-tpl.com/tpl_087/
--- hairsalon/views.py
+++ hairsalon/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from .models import News
+
 # Create your views here.
+
+def top(request):
+    news = News.objects.order_by('-published')
+    return render(request, 'hairsalon/top.html', {'news': news, 'current_menu_item': 'top'})
