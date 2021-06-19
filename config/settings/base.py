c28ddd2f326250a788e580637a735dde1006224d
nakarun <etd7crnt@gmail.com>
2021-06-19 18:27:20 +0900

    add point card page

--- config/settings/base.py
+++ config/settings/base.py
@@ -117,6 +117,8 @@ USE_TZ = True
 
 AUTH_USER_MODEL = 'hairsalon.BaseUser'
 
+LOGIN_URL = '/customer/login/'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
--- pointcard/views.py
+++ pointcard/views.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic.edit import CreateView
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from django.urls import reverse_lazy
 from django.contrib.auth import login
 from django.http import HttpResponseRedirect
+from django.shortcuts import render
 
 from .forms import LoginForm, SignUpForm
 
@@ -14,7 +17,7 @@ class CustomerSignUp(CreateView):
     object: object
     form_class = SignUpForm
     template_name = "pointcard/signup.html"
-    success_url = reverse_lazy('top')
+    success_url = reverse_lazy('customer_pointcard')
 
     def form_valid(self, form):
         """ログインも一緒にしてしまうためにオーバーライド"""
@@ -31,3 +34,12 @@ class CustomerLogout(LogoutView):
 class CustomerLogin(LoginView):
     form_class = LoginForm
     template_name = 'pointcard/login.html'
+
+
+class CustomerPointCard(LoginRequiredMixin, View):
+
+    def get(self, request, *args, **kwargs):
+        context = {
+            'stamp_frames': [0, 1, 2, 3, 4, 5],
+        }
+        return render(request, 'pointcard/pointcard.html', context)
