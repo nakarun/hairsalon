be1edc1cb93151789121e9c23a9ffc24d095f927
nakarun <etd7crnt@gmail.com>
2021-06-22 14:11:44 +0900

    実際にメールが送られるようにする

--- config/settings/production.py
+++ config/settings/production.py
@@ -4,8 +4,22 @@ from .base import *
 DEBUG = False
 ALLOWED_HOSTS = ['hairsalon']
 
-
 STATIC_ROOT = '/var/run/{}/static'.format(PROJECT_NAME)
 
 # Media files updated by users
 MEDIA_ROOT = '/var/run/{}/pics'.format(PROJECT_NAME)
+
+################
+# Authenticate #
+################
+
+# 実際にメールを送信
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+# メールサーバーへの接続設定
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
