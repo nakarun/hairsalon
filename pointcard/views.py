24f39baf50eb90a0def3fab2bb1815e5b9cf7e50
nakarun <etd7crnt@gmail.com>
2021-06-23 17:57:58 +0900

    リファクタ

--- pointcard/views.py
+++ pointcard/views.py
@@ -1,11 +1,24 @@
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.db.models import QuerySet
+from typing import Union, List
 
 from .models import PointCard, Stamp
 
 
 # Create your views here.
+
+def get_stamp_frames(pointcard: PointCard, stamps: Union[QuerySet, List[Stamp]]) -> List:
+
+    stamp_frames = [[0] * pointcard.horizontal_cells_count for i in range(pointcard.vertical_cells_count)]
+
+    for i, stamp in enumerate(stamps):
+        v = i // pointcard.horizontal_cells_count
+        h = i - v * pointcard.horizontal_cells_count
+        stamp_frames[v][h] = stamp
+
+    return stamp_frames
 
 
 class PointCardView(LoginRequiredMixin, View):
@@ -14,12 +27,7 @@ class PointCardView(LoginRequiredMixin, View):
         pointcard = PointCard.objects.get()
         stamps = Stamp.unused_objects.filter(customer=request.user)
 
-        stamp_frames = [[0] * pointcard.horizontal_cells_count for i in range(pointcard.vertical_cells_count)]
-
-        for i, stamp in enumerate(stamps):
-            v = i // pointcard.horizontal_cells_count
-            h = i - v * pointcard.horizontal_cells_count
-            stamp_frames[v][h] = stamp
+        stamp_frames = get_stamp_frames(pointcard, stamps)
 
         context = {
             'stamp_frames': stamp_frames,
