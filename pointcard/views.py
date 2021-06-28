1a66d523e44b2085dcf48a4f3584c23f2a55ba06
nakarun <39692090+nakarun@users.noreply.github.com>
2021-06-28 19:26:09 +0900

    viewクラスのリファクタリング (#11)

--- pointcard/views.py
+++ pointcard/views.py
@@ -1,6 +1,5 @@
-from django.views import View
+from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
 from django.db.models import QuerySet
 from typing import Union, List
 
@@ -10,30 +9,27 @@ from .models import PointCard, Stamp
 # Create your views here.
 
 def get_stamp_frames(pointcard: PointCard, stamps: Union[QuerySet, List[Stamp]]) -> List:
-
     stamp_frames = [[0] * pointcard.horizontal_cells_count for i in range(pointcard.vertical_cells_count)]
 
     for i, stamp in enumerate(stamps):
         v = i // pointcard.horizontal_cells_count
         h = i - v * pointcard.horizontal_cells_count
-        if v+1 > len(stamp_frames):
+        if v + 1 > len(stamp_frames):
             break
         stamp_frames[v][h] = stamp
 
     return stamp_frames
 
 
-class PointCardView(LoginRequiredMixin, View):
+class PointCardView(LoginRequiredMixin, TemplateView):
+    template_name = 'pointcard/pointcard.html'
 
-    def get(self, request, *args, **kwargs):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
         # TODO: salonでフィルターする
         pointcard = PointCard.objects.first()
-        stamps = Stamp.unused_objects.filter(customer=request.user)
+        stamps = Stamp.unused_objects.filter(customer=self.request.user)
 
-        stamp_frames = get_stamp_frames(pointcard, stamps)
-
-        context = {
-            'stamp_frames': stamp_frames,
-        }
-
-        return render(request, 'pointcard/pointcard.html', context)
+        context['stamp_frames'] = get_stamp_frames(pointcard, stamps)
+        return context
