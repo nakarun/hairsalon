333708f6ec5097161a97ffb2e4863281ccc71c1d
nakarun <etd7crnt@gmail.com>
2021-07-04 16:39:08 +0900

    VueRouterでbeforeEachを設定

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import store from "@/store";
 import LoginPage from "@/views/LoginPage.vue";
 import Home from "@/views/Home"
 
@@ -15,6 +16,11 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+    meta: { requiresAuth: true }
+  },
+  {
+    path: "/:catchAll(.*)",
+    redirect: "/"
   },
 ];
 
@@ -23,5 +29,61 @@ const router = new VueRouter({
   base: '/owner/',
   routes,
 });
+
+/**
+ * 画面遷移をする直前に毎回実行されるナビゲーションガード
+ */
+router.beforeEach((to, from, next) => {
+  const isLoggedIn = store.state.auth.isLoggedIn;
+  const token = localStorage.getItem("access");
+  console.log("to.path=", to.path);
+  console.log("isLoggedIn=", isLoggedIn);
+
+  // ログインが必要な画面に遷移しようとした場合
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    // ログインしていない場合
+    if (!isLoggedIn) {
+      console.log("User is not logged in.");
+      // まだ認証用トークンが残っていればユーザー情報を再取得
+      if (token != null) {
+        console.log("Try to renew user info.");
+        store
+          .dispatch("auth/renew")
+          .then(() => {
+            // 再取得できたらそのまま次へ
+            console.log("Succeeded to renew. So, free to next.");
+            next();
+          })
+          .catch(() => {
+            // 再取得できなければログイン画面へ
+            forceToLoginPage(to);
+          });
+      } else {
+        // 認証用トークンが無い場合は、ログイン画面へ
+        forceToLoginPage(to);
+      }
+    } else {
+      // ログインしている場合はそのまま次へ
+      console.log("User is already logged in. So, free to next.");
+      next();
+    }
+  } else {
+    // ログインが不要な画面であればそのまま次へ
+    console.log("Go to public page.");
+    next();
+  }
+});
+
+/**
+ * ログイン画面へ強制送還
+ */
+function forceToLoginPage(to) {
+  console.log("Force to login page.");
+  router.replace({
+    path: "/login",
+    // 遷移先のURLはクエリ文字列として付加
+    query: { next: to.fullPath }
+  });
+}
 
 export default router;
