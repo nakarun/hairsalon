534934140c0afe26c9072d4a2ba56eb7474f6adc
nakarun <etd7crnt@gmail.com>
2021-07-04 16:50:39 +0900

    Vuetifyを日本語化

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import ja from 'vuetify/es5/locale/ja.js'
 
 Vue.use(Vuetify)
 
-const opts = {}
+const opts = {
+    lang: {
+        locales: {ja},
+        current: 'ja'
+    }
+}
 
 export default new Vuetify(opts)
