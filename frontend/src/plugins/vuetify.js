163ac3a5556604439cd320e1d40dc5187aa88f8e
nakarun <etd7crnt@gmail.com>
2021-07-07 16:27:23 +0900

    vuetifyでiconを表示する

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -1,15 +1,19 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
-import ja from 'vuetify/es5/locale/ja.js'
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+import ja from 'vuetify/es5/locale/ja.js';
+import '@mdi/font/css/materialdesignicons.css';
 
-Vue.use(Vuetify)
+Vue.use(Vuetify);
 
 const opts = {
     lang: {
         locales: {ja},
         current: 'ja'
-    }
-}
+    },
+    icons: {
+      iconfont: 'mdi', // 追加
+    },
+};
 
-export default new Vuetify(opts)
+export default new Vuetify(opts);
