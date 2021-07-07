33158dab25ff2f4a4f26058d36cebff4ab49092f
nakarun <etd7crnt@gmail.com>
2021-07-07 17:35:17 +0900

    set navi var at login page

--- frontend/src/views/LoginPage.vue
+++ frontend/src/views/LoginPage.vue
@@ -1,6 +1,9 @@
 <template>
-  <main class="container mt-5 p-5">
-    <div id="login-page">
+  <div id="login-page">
+    <GlobalHeader />
+    <GlobalMessage />
+
+    <main class="container mt-5 p-5">
       <p class="h5 mb-5">Login</p>
       <v-form
         ref="form"
@@ -34,58 +37,65 @@
         </v-btn>
 
       </v-form>
-    </div>
-  </main>
+    </main>
+  </div>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      valid: true,
-      username: '',
-      usernameRules: [
-        v => !!v || 'ユーザー名を入力してください。',
-        v => (v && v.length <= 10) || 'ユーザー名は10文字以内です。',
-      ],
-      password: '',
-      passwordRules: [
-        v => !!v || 'パスワードを入力してください。',
-        v => (v && v.length <= 10) || 'パスワードは10文字以内です。',
-      ],
-    }),
+import GlobalHeader from "@/components/GlobalHeader.vue";
+import GlobalMessage from "@/components/GlobalMessage.vue";
 
-    methods: {
-      validate () {
-        if (this.$refs.form.validate()) {
-          this.submitLogin()
-        }
-      },
-      reset () {
-        this.$refs.form.reset()
-      },
-      resetValidation () {
-        this.$refs.form.resetValidation()
-      },
-      // ログインボタン押下
-      submitLogin: function() {
-        // ログイン実行
-        this.$store
-          .dispatch("auth/login", {
-            username: this.username,
-            password: this.password
-          })
-          .then(() => {
-            console.log("Login succeeded.");
-            this.$store.dispatch("message/setInfoMessage", {
-              message: "ログインしました。"
-            });
-            // クエリ文字列に「next」がなければ、ホーム画面へ
-            const next = this.$route.query.next || "/";
-            this.$router.replace(next);
-          });
+export default {
+  components: {
+    GlobalHeader,
+    GlobalMessage
+  },
+  data: () => ({
+    valid: true,
+    username: '',
+    usernameRules: [
+      v => !!v || 'ユーザー名を入力してください。',
+      v => (v && v.length <= 10) || 'ユーザー名は10文字以内です。',
+    ],
+    password: '',
+    passwordRules: [
+      v => !!v || 'パスワードを入力してください。',
+      v => (v && v.length <= 10) || 'パスワードは10文字以内です。',
+    ],
+  }),
+
+  methods: {
+    validate () {
+      if (this.$refs.form.validate()) {
+        this.submitLogin()
       }
     },
-  }
+    reset () {
+      this.$refs.form.reset()
+    },
+    resetValidation () {
+      this.$refs.form.resetValidation()
+    },
+    // ログインボタン押下
+    submitLogin: function() {
+      // ログイン実行
+      this.$store
+        .dispatch("auth/login", {
+          username: this.username,
+          password: this.password
+        })
+        .then(() => {
+          console.log("Login succeeded.");
+          this.$store.dispatch("message/setInfoMessage", {
+            message: "ログインしました。"
+          });
+          // クエリ文字列に「next」がなければ、ホーム画面へ
+          const next = this.$route.query.next || "/";
+          this.$router.replace(next);
+        });
+    }
+  },
+}
 </script>
 
 <style scoped>
